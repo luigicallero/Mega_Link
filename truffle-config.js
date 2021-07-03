67d970cc62a6a7991f16e3cf77e2164996965a00
luigicallero <58836287+luigicallero@users.noreply.github.com>
2021-07-03 00:34:24 -0300

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -32,11 +32,11 @@ module.exports = {
     matic: {
       // Public Mumbai RPC addresses
       //provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
-      //provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.maticvigil.com`),
+      //provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.maticvigil.com`), // worked fine on 2021-07-02
+      //network_id: 80001, // to be used with Public Polygon Mumbai rpc
       
       // Infura Mumbai RPC address
       provider: () => new HDWalletProvider(mnemonic, rpc_url),
-      //network_id: 80001, // to be used with Public Polygon Mumbai rpc
       network_id: 42, // to be used for Infura Polygon Mumbai rpc
       confirmations: 2,
       timeoutBlocks: 200,
